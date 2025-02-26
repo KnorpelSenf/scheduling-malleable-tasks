49acda20893adb4ad8b5f83b023e062fd6d81c53
KnorpelSenf <shtrog@gmail.com>
2025-02-26 11:20:39 +0000

    feat: add check for condition 3
    Co-authored-by: Karim Issa <e5kimol4ser@users.noreply.github.com>

--- src/algo.rs
+++ src/algo.rs
@@ -2,6 +2,7 @@ use itertools::Itertools;
 use std::hash::Hash;
 
 /// A problem instance
+#[derive(Debug)]
 pub struct Instance {
     /// The number of processors available
     pub processor_count: usize,
@@ -31,6 +32,7 @@ impl Job {
     }
 }
 /// Compares two values by their index
+#[derive(Debug)]
 pub struct Constraint(pub usize, pub usize);
 /// Implements a partial relation based on a list of constraints
 trait PartialRelation {
@@ -92,45 +94,37 @@ impl ScheduledJob {
 }
 
 #[derive(Clone, Debug)]
+/// A state in our dynamic program
 struct State {
-    len: usize,
-    ideal: Vec<Option<Job>>,
+    /// For each chain, how far have we advanced this chain
+    ideal: Vec<usize>,
+    /// For each chain, how many machines do we use for the front task, i.e. the job indicated by `ideal`
     allotment: Vec<usize>,
+    /// For each chain, how many machines do we use for the front task, i.e. the job indicated by `ideal`
     completion_times: Vec<i32>,
 }
 impl State {
-    fn empty(n: usize) -> Self {
+    fn empty(omega: usize) -> Self {
         State {
-            len: n,
-            ideal: vec![None; n],
-            allotment: vec![0; n],
-            completion_times: vec![0; n],
+            ideal: vec![0; omega],
+            allotment: vec![0; omega],
+            completion_times: vec![0; omega],
         }
     }
-    fn start_times(&self, i: usize) -> i32 {
-        self.ideal[i]
-            .as_ref()
-            .map(|ideal| self.completion_times[i] - ideal.processing_time(self.allotment[i]))
-            .unwrap_or(0)
-    }
-    fn is_valid(&self) -> bool {
-        // TODO: iterate completion times, check the number of available
-        // processors at each time
-
-        true
-    }
-    // fn try_add_job(&self, i: usize, job: Job) -> Option<Self> { None }
-    // fn can_add(&self, i: usize, job: Job) -> bool { false }
-    fn add_job(&self, i: usize, job: &Job, allot: usize, compl: i32) -> Self {
-        let len = self.len;
+    // fn start_times(&self, i: usize) -> i32 {
+    //     self.ideal[i]
+    //         .as_ref()
+    //         .map(|ideal| self.completion_times[i] - ideal.processing_time(self.allotment[i]))
+    //         .unwrap_or(0)
+    // }
+    fn add_job(&self, chain: usize, allot: usize, compl: i32) -> Self {
         let mut ideal = self.ideal.clone();
         let mut allotment = self.allotment.clone();
         let mut completion_times = self.completion_times.clone();
-        ideal[i] = Some(job.clone());
-        allotment[i] = allot;
-        completion_times[i] = compl;
+        ideal[chain] += 1;
+        allotment[chain] = allot;
+        completion_times[chain] = compl;
         State {
-            len,
             ideal,
             allotment,
             completion_times,
@@ -139,19 +133,16 @@ impl State {
 }
 impl Hash for State {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        for job in self.ideal.iter() {
-            if let Some(job) = job {
-                job.id.hash(state);
-            }
-        }
+        self.ideal.hash(state);
     }
 }
 
 pub fn schedule(instance: Instance) -> Schedule {
     let chains = preprocess(&instance);
-    let initial_state = State::empty(instance.jobs.len());
+    let omega = chains.len();
+    let initial_state = State::empty(omega);
 
-    let path = search(&instance, initial_state).expect("no solution found");
+    let path = search(&instance, &chains, initial_state).expect("no solution found");
     // path contains a list of indices in which to add jobs in order to
     // reach the target state
 
@@ -162,24 +153,56 @@ pub fn schedule(instance: Instance) -> Schedule {
     }
 }
 
-fn search(instance: &Instance, state: State) -> Option<Vec<usize>> {
-    if !state.is_valid() {
-        return None;
-    };
-    for i in 0..state.len {
-        // TODO: look this up in some state table in order to avoid searching
-        // the entire tree
-        let job = &instance.jobs[i];
-        for allot in 0..job.processing_times.len() {
-            for compl in 0..instance.max_time {
-                // TODO: check conditions 2 + 3
-                let new_state = state.add_job(i, job, allot, compl);
-                let tail = search(instance, new_state);
-                if let Some(tail) = tail {
-                    let mut path = Vec::with_capacity(tail.len() + 1);
-                    path.push(i);
-                    path.extend(tail);
-                    return Some(path);
+fn search(
+    instance: &Instance,
+    chains: &Vec<Vec<usize>>,
+    state: State,
+) -> Option<Vec<(usize, usize, i32)>> {
+    // TODO: look this up in some state table in order to avoid searching
+    // the entire tree
+
+    if state.ideal.len() + state.ideal.iter().sum::<usize>() == instance.jobs.len() {
+        return Some(vec![]);
+    }
+
+    for (chain_index, chain) in chains.iter().enumerate() {
+        let front_task_index = state.ideal[chain_index];
+        if front_task_index + 1 < chain.len() {
+            let job_index = chain[front_task_index];
+            let job = &instance.jobs[job_index];
+            for (&processing_time, allotment) in job.processing_times.iter().zip(1..) {
+                for compl in 0..instance.max_time {
+                    // TODO: iterate completion times, check the number of available
+                    // processors at each time
+
+                    let mut can_insert = true;
+
+                    // TODO: check condition 2
+
+                    let start_time = compl - job.processing_time(allotment);
+                    for (chain_index, &front_task_index) in state.ideal.iter().enumerate() {
+                        let job_index = chains[chain_index][front_task_index];
+                        let job = &instance.jobs[job_index];
+                        let completion_time = state.completion_times[chain_index];
+                        let processing_time = job.processing_time(state.allotment[chain_index]);
+                        if start_time < completion_time - processing_time {
+                            can_insert = false;
+                            break;
+                        }
+                    }
+
+                    if !can_insert {
+                        continue;
+                    }
+                    let new_state = state.add_job(chain_index, allotment, compl);
+
+                    let tail = search(instance, chains, new_state);
+                    if let Some(tail) = tail {
+                        let mut path = Vec::with_capacity(tail.len() + 1);
+                        path.push((job_index, allotment, compl));
+                        path.extend(tail);
+                        return Some(path);
+                    }
                 }
             }
         }
@@ -208,6 +231,7 @@ fn preprocess(instance: &Instance) -> Vec<Vec<usize>> {
         chains[i] = vec![];
         chains[j] = right;
     }
+    println!("preprocessed {:?} to chains {:?}", instance, chains);
     chains
 
     // TODO: fix the following attempt at writing a faster impl
