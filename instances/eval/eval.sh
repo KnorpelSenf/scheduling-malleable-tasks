c33b1718896650dd1e4b4f53377b1f1af944596c
KnorpelSenf <shtrog@gmail.com>
2025-06-03 11:51:03 +0000

    feat: eval timeouts

--- instances/eval/eval.sh
+++ instances/eval/eval.sh
@@ -4,12 +4,36 @@ set -ex
 
 cd "$(dirname "$0")"
 
-N=100
+JOB_FILE=./jobs.csv
+CONSTRAINT_FILE=./constraints.csv
+
+# m
+# min
+# max
+# omega
+# min-chain
+# max-chain
+
 echo "ms,n,m,makespan" | tee eval-{ilp,lp,dp}.csv
-for n in `seq 10 5 $N`
+for n in `seq 10 5 5000`
 do 
-    cargo run --release -- generate -n $n -m 4 --min 1 --max 10 -j jobs_basic_${n}.csv -o 8 --min-chain 1 --max-chain $n -c constraints_basic_${n}.csv --concave
-    cargo run --release -- solve-ilp -j jobs_basic_${n}.csv -c constraints_basic_${n}.csv >> eval-ilp.csv
-    cargo run --release -- solve-lp  -j jobs_basic_${n}.csv -c constraints_basic_${n}.csv >> eval-lp.csv
-    cargo run --release -- solve-dp  -j jobs_basic_${n}.csv -c constraints_basic_${n}.csv >> eval-dp.csv
+    cargo run -rq -- generate -j $JOB_FILE -c $CONSTRAINT_FILE -n $n -m 4 --min 1 --max 10 -o 8 --min-chain 1 --max-chain $n --concave
+    ILP_OUTPUT=$(timeout 15 cargo run -rq -- solve-ilp -j $JOB_FILE -c $CONSTRAINT_FILE 2&>/dev/null)
+    if [ $? == 124 ] 
+    then
+        break
+    fi
+    LP_OUTPUT=$(timeout 15 cargo run -rq -- solve-lp  -j $JOB_FILE -c $CONSTRAINT_FILE)
+    if [ $? == 124 ] 
+    then
+        break
+    fi
+    DP_OUTPUT=$(timeout 15 cargo run -rq -- solve-dp  -j $JOB_FILE -c $CONSTRAINT_FILE)
+    if [ $? == 124 ] 
+    then
+        break
+    fi
+    echo $ILP_OUTPUT >> eval-ilp.csv
+    echo $LP_OUTPUT  >> eval-lp.csv
+    echo $DP_OUTPUT  >> eval-dp.csv
 done
