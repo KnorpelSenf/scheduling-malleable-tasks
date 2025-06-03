5da96ed6556f5f4986b7f3e9662d1876594b690d
KnorpelSenf <shtrog@gmail.com>
2025-06-03 12:59:05 +0000

    refactor: extract N, move m to file name

--- instances/eval/eval.sh
+++ instances/eval/eval.sh
@@ -1,51 +1,54 @@
 #!/bin/bash
 
-set -ex
-
 cd "$(dirname "$0")"
 
+# currently no grid search for:
 # omega
 # min-chain
 # max-chain
 
+N=200
+
 for m in `seq 2 2 32`
 do
-    for max in {2,3,4,5,10,20,50,100}
+    for max in {2,3,4,5,10,20}
     do
+        echo "+++++++++++++++++ m $m max $max ++++++++++++++++++"
         INSTANCE_PATH=/results/instances
         RESULT_PATH=/results/results
         mkdir -p $INSTANCE_PATH $RESULT_PATH
         ILP_FILE=$RESULT_PATH/result-ilp-m${m}-max${max}.csv
         LP_FILE=$RESULT_PATH/result-lp-m${m}-max${max}.csv
         DP_FILE=$RESULT_PATH/result-dp-m${m}-max${max}.csv
-        for n in `seq 10 5 5000`
+        for n in `seq 10 5 $N`
         do 
-            cargo run -rq -- generate -j $INSTANCE_PATH/jobs-${n}.csv -c $INSTANCE_PATH/constraints-${n}.csv -n $n -m $m --min 1 --max $max -o 8 --min-chain 1 --max-chain $n --concave
+            cargo run -rq -- generate -j $INSTANCE_PATH/jobs-n${n}-m${m}.csv -c $INSTANCE_PATH/constraints-n${n}-m${m}.csv -n $n -m $m --min 1 --max $max -o 8 --min-chain 1 --max-chain $n --concave
         done
         echo "ms,n,m,makespan" | tee $ILP_FILE $LP_FILE $DP_FILE
-        for n in `seq 10 5 5000`
+        for n in `seq 10 5 $N`
         do 
-            timeout 15 cargo run -rq -- solve-ilp -j $INSTANCE_PATH/jobs-${n}.csv -c $INSTANCE_PATH/constraints-${n}.csv >> $ILP_FILE
+            time timeout 15 cargo run -rq -- solve-ilp -j $INSTANCE_PATH/jobs-n${n}-m${m}.csv -c $INSTANCE_PATH/constraints-n${n}-m${m}.csv >> $ILP_FILE
             if [ $? == 124 ] 
             then
                 break
             fi
         done
-        for n in `seq 10 5 5000`
+        for n in `seq 10 5 $N`
         do 
-            timeout 15 cargo run -rq -- solve-lp -j $INSTANCE_PATH/jobs-${n}.csv -c $INSTANCE_PATH/constraints-${n}.csv >> $LP_FILE
+            time timeout 15 cargo run -rq -- solve-lp -j $INSTANCE_PATH/jobs-n${n}-m${m}.csv -c $INSTANCE_PATH/constraints-n${n}-m${m}.csv >> $LP_FILE
             if [ $? == 124 ] 
             then
                 break
             fi
         done
-        for n in `seq 10 5 5000`
+        for n in `seq 10 5 $N`
         do 
-            timeout 15 cargo run -rq -- solve-dp -j $INSTANCE_PATH/jobs-${n}.csv -c $INSTANCE_PATH/constraints-${n}.csv >> $DP_FILE
+            time timeout 15 cargo run -rq -- solve-dp -j $INSTANCE_PATH/jobs-n${n}-m${m}.csv -c $INSTANCE_PATH/constraints-n${n}-m${m}.csv >> $DP_FILE
             if [ $? == 124 ] 
             then
                 break
             fi
         done
+        echo "----------------- m $m max $max ------------------"
     done
 done
