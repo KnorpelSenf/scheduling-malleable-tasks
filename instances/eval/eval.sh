760acf9726afe9543b0b1349b8b0195aa8600880
KnorpelSenf <shtrog@gmail.com>
2025-06-03 13:04:40 +0000

    style: drop trailing spaces

--- instances/eval/eval.sh
+++ instances/eval/eval.sh
@@ -25,30 +25,30 @@ eval_for_m() {
         DP_FILE=$RESULT_PATH/result-dp-m${m}-max${max}.csv
         # N is accessible here because it's exported
         for n in `seq 10 5 $N`
-        do 
+        do
             cargo run -rq -- generate -j $INSTANCE_PATH/jobs-n${n}-m${m}.csv -c $INSTANCE_PATH/constraints-n${n}-m${m}.csv -n $n -m $m --min 1 --max $max -o 8 --min-chain 1 --max-chain $n --concave
         done
         echo "ms,n,m,makespan" | tee $ILP_FILE $LP_FILE $DP_FILE
         for n in `seq 10 5 $N`
-        do 
+        do
             time timeout 15 cargo run -rq -- solve-ilp -j $INSTANCE_PATH/jobs-n${n}-m${m}.csv -c $INSTANCE_PATH/constraints-n${n}-m${m}.csv >> $ILP_FILE
-            if [ $? == 124 ] 
+            if [ $? == 124 ]
             then
                 break
             fi
         done
         for n in `seq 10 5 $N`
-        do 
+        do
             time timeout 15 cargo run -rq -- solve-lp -j $INSTANCE_PATH/jobs-n${n}-m${m}.csv -c $INSTANCE_PATH/constraints-n${n}-m${m}.csv >> $LP_FILE
-            if [ $? == 124 ] 
+            if [ $? == 124 ]
             then
                 break
             fi
         done
         for n in `seq 10 5 $N`
-        do 
+        do
             time timeout 15 cargo run -rq -- solve-dp -j $INSTANCE_PATH/jobs-n${n}-m${m}.csv -c $INSTANCE_PATH/constraints-n${n}-m${m}.csv >> $DP_FILE
-            if [ $? == 124 ] 
+            if [ $? == 124 ]
             then
                 break
             fi
